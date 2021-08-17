3a65ffdab7a70658db32156ada5f6a5c59815a90
Viktor Nyström <95viknys@skola.boras.se>
2021-08-17 18:17:57 +0200

    Added array of objects to start with

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 const express = require('express');
 const app = express();
 
+const products = 
+[
+    {
+        "id": 1,
+        "name": "towel",
+        "color": "red",
+        "price": 100,
+    },
+    {
+        "id": 2,
+        "name": "toy car",
+        "color": "blue",
+        "price": 50,
+    }
+]
+
+app.listen(3000, () => {
+    console.log('Server is running. . .');
+    console.log(products);
+});
