83f3b9112881c91aad89ba0eb56570ffd55cb633
Viktor Nyström <95viknys@skola.boras.se>
2021-08-18 13:34:58 +0200

    Router file created with all endpoints included

---
+++ products.router.js
@@ -0,0 +1,25 @@
+// Import modules
+const express = require('express');
+const {} = require('./plants.controllers');
+
+// Create router object
+const router = express.Router();
+
+// Define endpoints: 
+// GET
+router.get('/api/products', getProducts);
+
+// GET with Id
+router.get('api/products/:id', getProduct);
+
+// POST
+router.post('/api/products', postProduct);
+
+// PUT 
+router.put('/api/product/:id', putProduct);
+
+// DELETE
+router.delete('/api/product/:id', deleteProduct);
+
+// Export router object
+module.exports = router;
